af4254a91a4e7e3a6e2f07a750609d828a902581
Vladislav Nagaev <vladislav.nagaew@gmail.com>
2023-03-14 12:52:31 +0300

    changed docker-compose settings

--- entrypoint/jupyterlab-initialization.sh
+++ entrypoint/jupyterlab-initialization.sh
@@ -6,7 +6,7 @@ if [ "${COMMAND}" == "jupyterlab" ]; then
 
     echo -e "${blue_b}Starting Jupyter Lab ...${reset_font}";
 
-    jupyter lab --ip=0.0.0.0 --port=${SPARK_JUPYTERLAB_PORT:-8888} --no-browser --allow-root --NotebookApp.token="";
+    jupyter lab --ip=0.0.0.0 --port=${JUPYTERLAB_PORT:-8888} --no-browser --allow-root --NotebookApp.token="";
 
 fi;
 
--- entrypoint/jupyterlab-termination.sh
+++ entrypoint/jupyterlab-termination.sh
@@ -6,7 +6,7 @@ if [ "${COMMAND}" == "jupyterlab" ]; then
 
     echo -e "${blue_b}Ending Jupyter Lab ...${reset_font}";
 
-    jupyter lab stop ${SPARK_JUPYTERLAB_PORT:-8888};
+    jupyter lab stop ${JUPYTERLAB_PORT:-8888};
 
 fi;
 
